ba0eb9249a67c903725e3f1c7875a3beeca284b1
Adam Eivy <adam.eivy@disney.com>
2016-04-07 06:38:31 -0700

    move server.error to lib file

--- app/server/app.js
+++ app/server/app.js
@@ -128,29 +128,7 @@ var io = require('socket.io')(server);
  */
 server.listen(port);
 
-server.on('error', function(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-});
+server.on('error', require('./lib/server.error'));
 
 server.on('listening', function() {
   var addr = server.address();
