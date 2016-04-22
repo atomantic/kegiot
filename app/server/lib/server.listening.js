640aed52233f86a474f35420bd1af82917ecf8cc
Adam Eivy <adam.eivy@disney.com>
2016-04-22 09:53:01 -0700

    remove extra dep on `debug`

--- app/server/lib/server.listening.js
+++ app/server/lib/server.listening.js
@@ -1,10 +1,9 @@
-var debug = require('debug')('myapp:server');
 module.exports = function(server){
   return function() {
     var addr = server.address();
     var bind = typeof addr === 'string'
       ? 'pipe ' + addr
       : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+    console.log('Listening on ' + bind);
   };
 }
